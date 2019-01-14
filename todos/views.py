74603fdc7f45655152578acccfd8e6735b221fa7
jay51 <nenojaafar3@gmail.com>
2019-01-14 16:45:25 -0600

    added commints

--- todos/views.py
+++ todos/views.py
@@ -37,18 +37,21 @@ def create_todo(request):
         return render(request, "create_todo_form.html", {"form": form})
 
 
+# This will break if you delete the first todo in DB
+#  You need to pass todo id from urls.py and look for todos by id in DB
 def edit_todo(request):
     # first todo in DB
     todo = Todos.objects.get(id=1)
     # give the todo to form to poupliate form with data
     form = AddTodo(request.POST or None, instance=todo)
-    # when submit
+    # if form submit
     if request.method == "POST":
         if form.is_valid():
+            # save data to DB
             form.save()
     return render(request, "edit_todo.html", {"form": form})
 
-
+# id is passed from urls.py
 def dynamic_view(request, id):
     # fetch todo by id from DB
     # todo = Todos.objects.get(id=id)
@@ -56,11 +59,19 @@ def dynamic_view(request, id):
 
     return render(request, "lookup_todo.html", {"todo": todo})
 
-
+# Delete todo by id. Accepts only post requests
 def delete_todo(request, id):
     todo = Todos.objects.get(id=id)
-
+    # delete and redirect to home page
     if request.method == "POST":
         todo.delete()
 
     return redirect("/todos")
+
+
+"""
+Next start a new blog app 
+implment all views in new app as a class
+
+Create one layout.html for all views to extends
+"""
