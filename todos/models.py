5ca4193ca09c52bf32a09b9597a601b6206f2832
jay51 <nenojaafar3@gmail.com>
2019-01-16 03:38:26 -0600

    added more features just to show what djnago is capable of

--- todos/models.py
+++ todos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -6,3 +7,8 @@ class Todos(models.Model):
     Todo = models.CharField(max_length=120)
     summery = models.TextField(blank=True, null=True)
     done = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return f"/todos/{self.id}"  # will return the url with todo.id
+        # use reverse to make it dynamic based on the route name
+        # return reverse("index", kwargs={"id": self.id})
